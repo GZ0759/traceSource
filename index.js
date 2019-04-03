06232fdc3f5ed054a1477023efd601e29eead7dd
NCSA Mosaic <1239085033@qq.com>
2019-04-03 16:25:44 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  var mySwiper = new Swiper(".swiper-container", {
+    loop: true, // 循环模式选项
+
+    // 分页器
+    pagination: {
+      el: ".swiper-pagination"
+    }
+  });
+});
