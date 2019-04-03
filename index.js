e4f4b680542cca13d5c3148508bd2da6f499aa32
NCSA Mosaic <1239085033@qq.com>
2019-04-03 20:32:38 +0800

    finish

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   var mySwiper = new Swiper(".swiper-container", {
     loop: true, // 循环模式选项
-
+    autoplay: {
+      disableOnInteraction: false,
+    }, // 自动播放
     // 分页器
     pagination: {
       el: ".swiper-pagination"
-    }
+    },
   });
 });
